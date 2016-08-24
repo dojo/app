231c351415e6a4281c46eee475d0913950f65039
Kitson Kelly <me@kitsonkelly.com>
2016-08-24 14:08:47 +0100

    Comment out Safari for SauceLabs

--- tests/intern-saucelabs.ts
+++ tests/intern-saucelabs.ts
@@ -5,7 +5,7 @@ export const environments = [
 	{ browserName: 'microsoftedge', platform: 'Windows 10' },
 	{ browserName: 'firefox', version: '43', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
-	{ browserName: 'safari', version: '9', platform: 'OS X 10.11' },
+	// { browserName: 'safari', version: '9', platform: 'OS X 10.11' },
 	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' },
 	{ browserName: 'iphone', version: '7.1' }
 ];
