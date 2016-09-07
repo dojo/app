ca4b55af2f3b2e123465e8e677f9c78ee8c3a083
Anthony Gubler <agubler@sitepen.com>
2016-09-07 14:40:55 +0100

    added comment

--- tests/intern-saucelabs.ts
+++ tests/intern-saucelabs.ts
@@ -1,6 +1,8 @@
 export * from './intern';
 
 export const environments = [
+	// Currently Safari and Microsoft edge are causing the Saucelabs tunnel to crash, once the issues
+	// have been resolved these browsers/platforms can be re-added
 	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
 	{ browserName: 'firefox', version: '43', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
