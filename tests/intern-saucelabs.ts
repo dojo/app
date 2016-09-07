f8ecaea7b5d1abe978f950236e8d6ffcd11e16f4
Anthony Gubler <agubler@gmail.com>
2016-09-07 14:52:28 +0100

    remove failing saucelabs platforms (#52)
    * remove failing saucelabs platforms
    
    * added comment

--- tests/intern-saucelabs.ts
+++ tests/intern-saucelabs.ts
@@ -1,11 +1,11 @@
 export * from './intern';
 
 export const environments = [
+	// Currently Safari and Microsoft edge are causing the Saucelabs tunnel to crash, once the issues
+	// have been resolved these browsers/platforms can be re-added
 	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
-	{ browserName: 'microsoftedge', platform: 'Windows 10' },
 	{ browserName: 'firefox', version: '43', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
-	{ browserName: 'safari', version: '9', platform: 'OS X 10.11' },
 	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' },
 	{ browserName: 'iphone', version: '7.1' }
 ];
