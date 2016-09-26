dojo/app

commit d0079cfd61befd6a4ff3e9ad79008596c7af537e

Dynmically create widget for the type set in state (#55)
* Add support for factorying widgets based on type
* options requires registry provider, stateFrom and id properties

Fixes #54

src/createApp.ts
@@ -39,6 +39,7 @@ export type ActionLike = Action<any, any, any>;
  */
 export type StoreLike = ObservableState<State> & {
 	add<T>(item: T, options?: any): Promise<T>;
+	get<T>(id: string): Promise<T>;
 }
 
 /**
@@ -512,6 +513,22 @@ function addIdentifier(app: App, id: Identifier) {
 	};
 }
 
+function createCustomWidget(app: App, id: string) {
+	const customFactories = customElementFactories.get(app);
+	const { registryProvider, defaultActionStore: stateFrom } = app;
+
+	return app.defaultWidgetStore.get(id).then((state: any) => {
+		const options: any = { id, stateFrom, registryProvider, state };
+		const customFactory = customFactories.get(state.type);
+		const widgetOrPromise = customFactory(options);
+
+		return Promise.resolve(widgetOrPromise).then((widget) => {
+			widget.own(registerInstance(app, widget, id));
+			return widget;
+		});
+	});
+}
+
 function registerInstance(app: App, instance: WidgetLike, id: string): Handle {
 	// Maps the instance to its ID
 	const instanceHandle = instanceRegistries.get(app).addWidget(instance, id);
@@ -899,26 +916,21 @@ const createApp = compose({
 			// custom elements.
 			const factories = widgetFactories.get(this);
 			const instances = widgetInstances.get(this);
-			return new Promise((resolve) => {
-				// First see if a factory exists for the widget.
-				let factory: WidgetFactory;
-				try {
-					factory = factories.get(id);
-				} catch (missingFactory) {
-					try {
-						// Otherwise try and get an existing instance.
-						const instance = instances.get(id);
-						resolve(instance);
-						return; // Make sure to return!
-					} catch (_) {
-						// Don't confuse people by complaining about missing instances, rethrow the
-						// original error.
-						throw missingFactory;
-					}
-				}
-				// This is only reached when a factory exists. Call it and resolve with the result.
-				// If it throws that's fine, it'll reject the promise.
-				resolve(factory());
+
+			let missingFactory: any;
+			return Promise.resolve().then(() => {
+				const factory = factories.get(id);
+				return factory();
+			})
+			.catch((e) => {
+				missingFactory = e;
+				return instances.get(id);
+			})
+			.catch((e) => {
+				return createCustomWidget(this, id);
+			})
+			.catch((e) => {
+				throw missingFactory;
 			});
 		},
 

tests/support/createApp.ts
@@ -1,6 +1,7 @@
 import Promise from 'dojo-shim/Promise';
 import * as assert from 'intern/chai!assert';
 import compose from 'dojo-compose/compose';
+import Map from 'dojo-shim/Map';
 import { h } from 'maquette';
 import { Handle } from 'dojo-core/interfaces';
 
@@ -23,9 +24,17 @@ export function createStore(): StoreLike {
 export const createSpyStore = compose({
 	add(this: any, ...args: any[]): Promise<any> {
 		this._add.push(args);
+		const id = args ? args[0].id : null;
+		if (id) {
+			this._map.set(id, args[0]);
+		}
 		return Promise.resolve({});
 	},
+	get(this: any, id: string): Promise<any> {
+		return Promise.resolve(this._map.get(id));
+	},
 	_add: <any[][]> null,
+	_map: <Map<string, any>> null,
 	observe(...args: any[]): any {},
 	_observe: <any[][]> null,
 	patch(this: any, ...args: any[]): Promise<any> {
@@ -37,6 +46,7 @@ export const createSpyStore = compose({
 }, (instance, options) => {
 	instance._options = options;
 	instance._add = [];
+	instance._map = new Map<string, any>();
 	instance._observe = [];
 	instance._patch = [];
 });
@@ -66,6 +76,12 @@ export const createSpyWidget = compose({
 	instance._options = options;
 	instance._own = [];
 });
+
+export function createAsyncSpyWidget(): Promise<WidgetLike> {
+	return new Promise<WidgetLike>((resolve) => {
+		resolve(createSpyWidget());
+	});
+}
 
 export function invert(promise: Promise<any>): Promise<any> {
 	return promise.then((value) => {

tests/unit/createApp/widgets.ts
@@ -16,6 +16,7 @@ import {
 	createAction,
 	createStore,
 	createWidget,
+	createAsyncSpyWidget,
 	createSpyStore,
 	createSpyWidget,
 	invert,
@@ -40,6 +41,63 @@ registerSuite({
 			app.registerWidget('foo', expected);
 
 			return strictEqual(app.getWidget('foo'), expected);
+		},
+
+		'creates and provides widget using factory registered for the states custom type'() {
+			const defaultWidgetStore = createSpyStore();
+			const app = createApp({ defaultWidgetStore });
+
+			app.registerCustomElementFactory('custom-element', createSpyWidget);
+
+			return defaultWidgetStore.add({id: 'foo', type: 'custom-element'}).then(() => {
+				return app.getWidget('foo').then((widget) => {
+					assert.isObject(widget);
+				});
+			});
+		},
+
+		'creates (async) and provides widget using factory registered for the states custom type'() {
+			const defaultWidgetStore = createSpyStore();
+			const app = createApp({ defaultWidgetStore });
+
+			app.registerCustomElementFactory('custom-element', createAsyncSpyWidget);
+
+			return defaultWidgetStore.add({id: 'foo', type: 'custom-element'}).then(() => {
+				return app.getWidget('foo').then((widget) => {
+					assert.isObject(widget);
+				});
+			});
+		},
+
+		'no registered factory for the widget state type'() {
+			const defaultWidgetStore = createSpyStore();
+			const app = createApp({ defaultWidgetStore });
+
+			return defaultWidgetStore.add({id: 'foo', type: 'custom-element'}).then(() => {
+				return rejects(app.getWidget('foo'), Error);
+			});
+		},
+
+		'no default widget store'() {
+			const app = createApp();
+
+			return rejects(app.getWidget('foo'), Error);
+		},
+
+		'no state record for id'() {
+			const defaultWidgetStore = createSpyStore();
+			const app = createApp({ defaultWidgetStore });
+
+			return rejects(app.getWidget('foo'), Error);
+		},
+
+		'no type on state record'() {
+			const defaultWidgetStore = createSpyStore();
+			const app = createApp({ defaultWidgetStore });
+
+			return defaultWidgetStore.add({id: 'foo'}).then(() => {
+				return rejects(app.getWidget('foo'), Error);
+			});
 		}
 	},
 

commit a78dc07f6bbc10c0e73e13cd221c2b9eb11e6d3a

Restore 100% code coverage, minor tweaks to #55 (#59)
* Restore most of the original getWidget implementation

Coverage for the else-branch of
<https://github.com/dojo/app/blob/9dc8c03b1fe63b9054aca017ca47fd4fe3c2dbee/src/createApp.ts#L753:L755>
broke (see
<https://github.com/dojo/app/blob/9dc8c03b1fe63b9054aca017ca47fd4fe3c2dbee/tests/unit/createApp/widgets.ts#L425>)
because the registry was no longer called synchronously (see #55). Of
course the test could have been amended, though the promise interaction
would've been tricky.

Restoring most of the original implementation surfaced another error
that was swallowed by the `.catch()` chaining introduced in #55.
createCustomWidget() would crash if there was no default widget store.
I've added a guard to ensure createCustomWidget() is only called if
there is a default store.

* Minor createCustomWidget improvement

Since customFactory is called in a promise callback we can simply return
and chain the next callback, without having to use Promise.resolve().

src/createApp.ts
@@ -519,12 +519,10 @@ function createCustomWidget(app: App, id: string) {
 	return app.defaultWidgetStore.get(id).then((state: any) => {
 		const options: any = { id, stateFrom, registryProvider, state };
 		const customFactory = customFactories.get(state.type);
-		const widgetOrPromise = customFactory(options);
-
-		return Promise.resolve(widgetOrPromise).then((widget) => {
-			widget.own(registerInstance(app, widget, id));
-			return widget;
-		});
+		return customFactory(options);
+	}).then((widget) => {
+		widget.own(registerInstance(app, widget, id));
+		return widget;
 	});
 }
 
@@ -916,20 +914,34 @@ const createApp = compose({
 			const factories = widgetFactories.get(this);
 			const instances = widgetInstances.get(this);
 
-			let missingFactory: any;
-			return Promise.resolve().then(() => {
-				const factory = factories.get(id);
-				return factory();
-			})
-			.catch((e) => {
-				missingFactory = e;
-				return instances.get(id);
-			})
-			.catch((e) => {
-				return createCustomWidget(this, id);
-			})
-			.catch((e) => {
-				throw missingFactory;
+			return new Promise((resolve) => {
+				// First see if a factory exists for the widget.
+				let factory: WidgetFactory;
+				let result: WidgetLike | Promise<WidgetLike>;
+				try {
+					factory = factories.get(id);
+				} catch (missingFactory) {
+					try {
+						// Otherwise try and get an existing instance.
+						result = instances.get(id);
+					} catch (_) {
+						// Try creating a custom widget, but only if there is a default widget store. The default
+						// widget store should contain details on how to create the widget.
+						if (this.defaultWidgetStore) {
+							result = createCustomWidget(this, id)
+								// Rethrow the original error since it's probably the least confusing one.
+								.catch(() => { throw missingFactory; });
+						}
+						else {
+							// Reject with the original error since it's probably the least confusing one.
+							result = Promise.reject(missingFactory);
+						}
+					}
+				}
+
+				// Resolve with the result, or call the factory and resolve with its result. If the factory throws
+				// that's fine, the returned promise will reject with an appropriate error.
+				resolve(result || factory());
 			});
 		},
 