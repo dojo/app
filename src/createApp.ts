da9fdf56511645ce5c907e1c7f649d09e48709e3
Mark Wubben <mark@novemberborn.net>
2016-09-30 11:53:27 +0100

    Refactor custom widget creation
    Explicitly guard against there not being a default widget store, to
    comply with strict null checks.
    
    Reorder some code and change some variables.

--- src/createApp.ts
+++ src/createApp.ts
@@ -568,12 +568,15 @@ function addIdentifier(app: App, id: Identifier) {
 }
 
 function createCustomWidget(app: App, id: string) {
-	const customFactories = customElementFactories.get(app);
-	const { registryProvider, defaultWidgetStore: stateFrom } = app;
+	const { registryProvider, defaultWidgetStore } = app;
+	// istanbul ignore if
+	if (!defaultWidgetStore) {
+		throw new Error('A default widget store must be configured in order to create custom widgets');
+	}
 
-	return app.defaultWidgetStore.get(id).then((state: any) => {
-		const options: WidgetFactoryOptions = { id, stateFrom, registryProvider, state };
-		const customFactory = customFactories.get(state.type);
+	return defaultWidgetStore.get(id).then((state: any) => {
+		const options: WidgetFactoryOptions = { id, stateFrom: defaultWidgetStore, registryProvider, state };
+		const customFactory = customElementFactories.get(app).get(state.type);
 		return customFactory(options);
 	}).then((widget) => {
 		widget.own(registerInstance(app, widget, id));
