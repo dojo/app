a34040ca212c20cc39a3b790258521313e904212
Anthony Gubler <agubler@sitepen.com>
2016-10-03 10:50:39 +0100

    support 'data-css-transitions' option for app-projector markup

--- src/lib/realizeCustomElements.ts
+++ src/lib/realizeCustomElements.ts
@@ -234,6 +234,10 @@ function resolveOptions(registry: ReadOnlyRegistry, registryProvider: RegistryPr
 	return options;
 }
 
+function getTransitionOptionFromProjector(element: Element): boolean {
+	return Boolean(element.getAttribute('data-css-transitions'));
+}
+
 function resolveStateFromAttribute(registry: ReadOnlyRegistry, element: Element): Promise<StoreLike> {
 	const stateFrom = element.getAttribute('data-state-from');
 	return stateFrom ? registry.getStore(stateFrom) : null;
@@ -290,7 +294,8 @@ export default function realizeCustomElements(
 
 		const widgetProjectors = getCustomElementsByWidgetProjector(registry, root);
 		for (const { children, element: root } of widgetProjectors) {
-			const projector = createProjector({ root });
+			const cssTransitions = getTransitionOptionFromProjector(root);
+			const projector = createProjector({ root, cssTransitions });
 			immediatePlaceholderLookup.set(projector, children);
 			projectors.push(projector);
 
