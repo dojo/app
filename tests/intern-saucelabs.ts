2abda8f4547f82a64133ece81d04093c5cff581e
Anthony Gubler <agubler@gmail.com>
2016-10-14 13:08:59 +0100

    update ios version for tests (#81)
    * update ios version for tests
    
    * remove ios as causes saucelabs to crash

--- tests/intern-saucelabs.ts
+++ tests/intern-saucelabs.ts
@@ -1,13 +1,12 @@
 export * from './intern';
 
 export const environments = [
-	// Currently Safari and Microsoft edge are causing the Saucelabs tunnel to crash, once the issues
+	// Currently ios (9.3), Safari and Microsoft edge are causing the Saucelabs tunnel to crash, once the issues
 	// have been resolved these browsers/platforms can be re-added
 	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
 	{ browserName: 'firefox', version: '43', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
-	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' },
-	{ browserName: 'iphone', version: '7.1' }
+	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' }
 ];
 
 /* SauceLabs supports more max concurrency */
