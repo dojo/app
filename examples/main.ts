4200ffb6381b658ef40db7e2c4b65aeb2560a5c0
Mark Wubben <mark@novemberborn.net>
2016-10-24 14:10:02 +0100

    Support updating (contrived) scenes

--- examples/main.ts
+++ examples/main.ts
@@ -1,3 +1,4 @@
+import global from 'dojo-core/global';
 import createWidget from 'dojo-widgets/createWidget';
 
 import createApp from '../src/createApp';
@@ -5,16 +6,35 @@ import createApp from '../src/createApp';
 const app = createApp();
 
 app.registerWidget('foo', createWidget({ state: { label: 'foo' }, tagName: 'h1' }));
+app.registerWidget('bar', createWidget({ state: { label: 'bar' }, tagName: 'h1' }));
 
-app.renderScene({
-	// FIXME: App must be created with the root element, disallow it from changing between scenes.
-	root: document.body,
-	tree: {
-		projector: true,
-		append: [
-			{
-				widget: 'foo'
-			}
-		]
-	}
-});
+global.renderFoo = () => {
+	app.renderScene({
+		// FIXME: App must be created with the root element, disallow it from changing between scenes.
+		root: document.body,
+		tree: {
+			projector: true,
+			append: [
+				{
+					widget: 'foo'
+				}
+			]
+		}
+	});
+};
+
+global.renderBar = () => {
+	app.renderScene({
+		root: document.body,
+		tree: {
+			projector: true,
+			append: [
+				{
+					widget: 'bar'
+				}
+			]
+		}
+	});
+};
+
+global.renderFoo();
--- src/lib/sceneManagement.ts
+++ src/lib/sceneManagement.ts
@@ -1,8 +1,10 @@
 import { Handle } from 'dojo-core/interfaces';
+import { includes } from 'dojo-shim/array';
 import Map from 'dojo-shim/Map';
 import Promise from 'dojo-shim/Promise';
 import WeakMap from 'dojo-shim/WeakMap';
 import { createProjector, Projector } from 'dojo-widgets/projector';
+import { List } from 'immutable';
 
 import {
 	Identifier,
@@ -36,6 +38,7 @@ interface Counter {
 
 interface RenderState {
 	root: Element;
+	children: WeakMap<Key<any>, Key<any>[]>;
 	handles: WeakMap<Key<any>, Handle>;
 	projectorKeys: Map<number, Key<number>>;
 	projectors: WeakMap<Key<number>, Projector>;
@@ -52,6 +55,7 @@ export function render(registry: ReadOnlyRegistry, root: Element, tree: SceneEle
 
 	const state = renderState.get(registry) || {
 		root,
+		children: new WeakMap<Key<any>, Key<any>[]>(),
 		handles: new WeakMap<Key<any>, Handle>(),
 		projectorKeys: new Map<number, Key<number>>(),
 		projectors: new WeakMap<Key<number>, Projector>(),
@@ -97,6 +101,7 @@ function update(
 	const projector = state.projectors.get(projectorKey) || createProjector({ root: state.root });
 	if (!state.projectors.has(projectorKey)) {
 		state.projectors.set(projectorKey, projector);
+		state.children.set(projectorKey, []);
 		state.handles.set(projectorKey, {
 			destroy() {
 				state.projectorKeys.delete(projectorKey.value);
@@ -104,7 +109,7 @@ function update(
 		});
 	}
 
-	return updateProjectorChildren(registry, state, counter, projector, tree.append)
+	return updateProjectorChildren(registry, state, counter, projectorKey, projector, tree.append)
 		.then(() => {
 			projector.attach();
 		});
@@ -114,28 +119,53 @@ function updateProjectorChildren(
 	registry: ReadOnlyRegistry,
 	state: RenderState,
 	counter: Counter,
+	projectorKey: Key<number>,
 	projector: Projector,
 	append: SceneWidget[]
 ): Promise<void> {
-	return Promise.all(
-		append.map(({ widget: id }) => registry.getWidget(id))
-	).then((widgets) => {
-		for (const widget of widgets) {
-			const { id } = widget;
-			const key = state.widgetKeys.get(id) || { value: id };
-			if (!state.widgetKeys.has(id)) {
-				state.widgetKeys.set(id, key);
-			}
+	interface Child {
+		key: Key<Identifier>;
+		widget: WidgetLike;
+	}
+	const children: (Promise<Child> | Child)[] = append.map(({ widget: value }) => {
+		const key = state.widgetKeys.get(value) || { value };
+		if (!state.widgetKeys.has(value)) {
+			state.widgetKeys.set(value, key);
+		}
 
-			// TODO: Implement diffing…
+		const widget = state.widgets.get(key);
+		if (widget) {
+			return { key, widget };
+		}
+
+		return registry.getWidget(value).then((widget) => {
 			state.widgets.set(key, widget);
-			const handle = projector.append(widget);
 			state.handles.set(key, {
 				destroy() {
-					handle.destroy();
+					widget.destroy();
 					state.widgetKeys.delete(key.value);
 				}
 			});
-		}
+			return { key, widget };
+		});
 	});
+
+	return Promise.all(children)
+		.then((children) => {
+			const newKeys = children.map(({ key }) => key);
+			const previousKeys = state.children.get(projectorKey);
+			for (const key of previousKeys) {
+				if (!includes(newKeys, key)) {
+					// TODO: It follows that any registered widget instances should override their destroy() method, as
+					// destruction would prevent them from being reused. Alternatively we could communicate destroyability through
+					// the registry.
+					//
+					// Also, upon destruction factories should revert to creating a new instance. And if a factory always returns
+					// the same instance then the above applies.
+					state.handles.get(key).destroy();
+				}
+			}
+
+			projector.children = List(children.map(({ widget }) => widget));
+		});
 }
